8197a796a17706aee558c67b5c3958d92dcb3eb3
IgorBern02 <igor.bernardes2@gmail.com>
2023-08-20 23:58:30 -0300

    api

--- script/script.js
+++ script/script.js
@@ -8,11 +8,48 @@
 //     }
 // }
 
-$('#searchImg').on('click', () => {
-    if(inputSearch.style.display === "none"){
-        inputSearch.style.display = "flex",
-        inputSearch.style.opacity = "1"
-        } else {
-        inputSearch.style.display = "none"
-        }
+// $("#searchImg").on("click", () => {
+//     if(inputSearch.style.display === "none"){
+//         inputSearch.style.display = "flex",
+//         $("#inputSearch").style.transition.display = "all 2s";
+//         } else {
+//         inputSearch.style.display = "none"
+//         }
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let products = document.querySelector(".products");
+  async function fetchProducts(url) {
+    let data = await fetch(url);
+    let response = await data.json();
+
+    for (let i = 0; i < response.length; i++) {
+      let description = response[i].description;
+      let title = response[i].title;
+      products.innerHTML += ` 
+        <div class="product">
+          <img src="${response[i].images[1]}" class="product-img" alt="..." />
+            
+                <h5 class="product-title">${
+                  title.length > 18
+                    ? title.substring(0, 18).concat("...")
+                    : title
+                }</h5>
+                <p class="product-description">${
+                  description.length > 80
+                    ? description.substring(0, 80).concat("...more")
+                    : description
+                }</p>
+                <div class="product-price-container">
+                <h3 class="product-price">${response[i].price}</h3>
+                <a href="#" data-productId="${
+                  response[i].id
+                }" class="add-to-cart"></a>
+                </div>
+            
+        </div>
+        `;
+    }
+  }
+  fetchProducts("https://api.escuelajs.co/api/v1/products");
 });
